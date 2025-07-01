c641e77b0d49f67a95e7566acc286cc0c330ee87
GuilhermeCosta-Tech <guilhermecosta.tech@gmail.com>
2025-06-30 22:46:24 -0300

    feat(database): implementa DbContext, Repository e Interface

--- JwInventory.API/Program.cs
+++ JwInventory.API/Program.cs
@@ -1,25 +1,85 @@
+using JwInventory.Infrastructure.Data;
+using JwInventory.Infrastructure.Repositories;
+using JwInventory.Infrastructure.Repositories.Interfaces;
+using JwInventory.Infrastructure.Services;
+using JwInventory.Infrastructure.Security;
+using JwInventory.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configurar serviços
 builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// JWT config
+var key = builder.Configuration["JwtConfig:Key"];
+var issuer = builder.Configuration["JwtConfig:Issuer"];
+var audience = builder.Configuration["JwtConfig:Audience"];
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = issuer,
+        ValidateAudience = true,
+        ValidAudience = audience,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+        ValidateLifetime = true
+    };
+});
+
+// Injetar repositórios e serviços
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<JwtTokenGenerator>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// forçar o uso de HTTPS /*swagger não funcionou como deveria*/
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/swagger");
+    return Task.CompletedTask;
+});
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
---
+++ JwInventory.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using JwInventory.Domain.Entities;
+
+namespace JwInventory.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>(); // TPH (herança)
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasDiscriminator<string>("UserType")
+                .HasValue<AdminUser>("Admin")
+                .HasValue<ManagerUser>("Gerente")
+                .HasValue<EmployeeUser>("Colaborador");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
