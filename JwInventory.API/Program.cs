b26c3d49d0bec94a0df39b35d99490d881526a80
Guilherme <guilhermecosta.tech@gmail.com>
2025-07-05 20:50:42 -0300

    Revert "feat(auth): configuração inicial de JWT com bcrypt e endpoints Swagger"

--- JwInventory.API/Program.cs
+++ JwInventory.API/Program.cs
@@ -1,83 +1,25 @@
-using JwInventory.Infrastructure.Data;
-using JwInventory.Infrastructure.Repositories;
-using JwInventory.Infrastructure.Repositories.Interfaces;
-using JwInventory.Infrastructure.Services;
-using JwInventory.Infrastructure.Security;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using JwInventory.Domain.Interfaces.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// DB Context
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// JWT Authentication
-var key = builder.Configuration["JwtConfig:Key"];
-var issuer = builder.Configuration["JwtConfig:Issuer"];
-var audience = builder.Configuration["JwtConfig:Audience"];
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = false;
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidIssuer = issuer,
-        ValidateAudience = true,
-        ValidAudience = audience,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
-        ValidateLifetime = true
-    };
-});
-
-// Injeção de dependência
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-
-builder.Services.AddScoped<JwtTokenGenerator>();
+// Add services to the container.
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Middleware
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
-
-// Redirecionar / para /swagger
-app.MapGet("/", context =>
-{
-    context.Response.Redirect("/swagger");
-    return Task.CompletedTask;
-});
+app.MapRazorPages();
 
 app.Run();
